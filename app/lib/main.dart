47b92fdbee049a26a500f050635364bca23f2980
Adrieldf <adriel.idf@gmail.com>
2020-06-01 19:50:09 -0300

    test commit

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -108,7 +108,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   Row(
                     children: [
                       Text(
-                        "Nome",
+                        "Nome ",
                         style: TextStyle(color: Colors.white),
                       )
                     ],
