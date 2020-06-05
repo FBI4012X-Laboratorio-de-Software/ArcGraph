a519889b6732ac5332d896dcef551882c15ce804
yagobrandalise <brandaliseyago@gmail.com>
2020-06-04 22:17:17 -0300

    attempt to read from file

---
+++ app/lib/csvReader.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:async';
+import 'package:csv/csv.dart';
+import 'package:path_provider/path_provider.dart';
+
+
+
+class CsvReader {
+  Future<String> get localPath async {
+    final dir = await getApplicationDocumentsDirectory();
+    return dir.path;
+  }
+
+  Future<File> get localFile async {
+    final path = await localPath;
+    return File('$path/teste.csv');
+  }
+
+  void readData() async {
+    final path = await localPath;
+    final input = new File('$path/teste.csv').openRead();
+    final csv = await input.transform(utf8.decoder).transform(new CsvToListConverter()).toList();
+    print(csv);
+    }
+  }
